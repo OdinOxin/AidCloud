db8af0a37c0b2edb8c6bb50da4ed8e44c1064b21
OdinOxin <OdinOxin@googlemail.com>
2016-11-29 09:09:31 +0100

    Seperated Port

--- src/main/java/de/odinoxin/aidcloud/AidCloud.java
+++ src/main/java/de/odinoxin/aidcloud/AidCloud.java
@@ -25,7 +25,7 @@ import java.util.Enumeration;
 public class AidCloud extends Application {
     public static final String INVALID_SESSION = "Invalid session, or session is over!";
     private static final String ADDRESS_PREFIX = "http://";
-    private static final String ADDRESS = ADDRESS_PREFIX + "%s/AidCloud/";
+    private static final String ADDRESS = ADDRESS_PREFIX + "%s:%s/AidCloud/";
     private static final int PORT = 15123;
 
     public static void main(String[] args) {
@@ -39,11 +39,31 @@ public class AidCloud extends Application {
         GridPane root = FXMLLoader.load(AidCloud.class.getResource("/AidCloud.fxml"));
 
         TextField txfURL = (TextField) root.lookup("#txfURL");
+        TextField txfPort = (TextField) root.lookup("#txfPort");
         TextField txfDBURL = (TextField) root.lookup("#txfDBURL");
         TextField txfDB = (TextField) root.lookup("#txfDB");
         ComboBox<DBSetting> cboDB = (ComboBox<DBSetting>) root.lookup("#cboDB");
         TextField txfUser = (TextField) root.lookup("#txfUser");
         PasswordField pwfPwd = (PasswordField) root.lookup("#pwfPwd");
+
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface networkInterface;
+            Enumeration<InetAddress> addresses;
+            while (networkInterfaces.hasMoreElements()) {
+                networkInterface = networkInterfaces.nextElement();
+                if (networkInterface.isLoopback() || !networkInterface.isUp())
+                    continue;
+                addresses = networkInterface.getInetAddresses();
+                if (addresses.hasMoreElements()) {
+                    txfURL.setText(addresses.nextElement().getHostAddress());
+                    break;
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        txfPort.setText(String.valueOf(AidCloud.PORT));
 
         txfDBURL.setText("localhost");
         txfDB.setText("AidCloud");
@@ -63,20 +83,6 @@ public class AidCloud extends Application {
                 new DBSetting("MSSQL 2012", "com.microsoft.sqlserver.jdbc.SQLServerDriver", "org.hibernate.dialect.SQLServer2012Dialect", "jdbc:sqlserver://"));
         cboDB.getSelectionModel().selectFirst();
 
-        try {
-            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-            Enumeration<InetAddress> addresses;
-            while (networkInterfaces.hasMoreElements()) {
-                addresses = networkInterfaces.nextElement().getInetAddresses();
-                while (addresses.hasMoreElements()) {
-                    String adr = addresses.nextElement().getHostAddress() + (AidCloud.PORT != 80 ? ":" + AidCloud.PORT : "");
-                    if ((txfURL.getText() == null || txfURL.getText().isEmpty()) && !adr.startsWith("127.") && !adr.startsWith("0:"))
-                        txfURL.setText(adr);
-                }
-            }
-        } catch (SocketException ex) {
-            ex.printStackTrace();
-        }
         Button btnLaunch = (Button) root.lookup("#btnLaunch");
         btnLaunch.setOnAction(ev -> {
             Scene setup = primaryStage.getScene();
@@ -101,7 +107,10 @@ public class AidCloud extends Application {
                         url = url.substring(AidCloud.ADDRESS_PREFIX.length());
                     if (url.endsWith("/"))
                         url = url.substring(0, url.length() - 1);
-                    String adr = String.format(AidCloud.ADDRESS, txfURL.getText());
+                    String port = txfPort.getText();
+                    if (port == null || port.isEmpty())
+                        port = String.valueOf(AidCloud.PORT);
+                    String adr = String.format(AidCloud.ADDRESS, url, port);
                     Endpoint.publish(adr + "Login", new Login());
                     Endpoint.publish(adr + "LanguageProvider", new LanguageProvider());
                     Endpoint.publish(adr + "Translator", Translator.get());
@@ -121,6 +130,7 @@ public class AidCloud extends Application {
                     ex.printStackTrace();
                     Platform.runLater(() -> {
                         primaryStage.setScene(setup);
+                        new Alert(Alert.AlertType.ERROR, ex.getLocalizedMessage(), ButtonType.OK).showAndWait();
                     });
                 }
             }).start();
